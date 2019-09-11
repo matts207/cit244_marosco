8cc1720758a25d0cc0474dd14326148b4b620707
MaRoSco <matts207@live.com>
2019-09-11 16:06:35 -0400

    fixed a git issue

---
+++ Object Models/src/objectmodels/Automobile.java
@@ -0,0 +1,226 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectmodels;
+
+/**
+ *
+ * @author matts207
+ */
+public class Automobile {
+    private String vinNumber;
+    private String engineType;
+    private int numberOfWheels;
+    private int numberOfSeats;
+    private double cargoCapacity;
+    private int range;
+    private String transmissionType;
+    private String driveTrain;
+    private int numberOfGears;
+    private int numberOfDoors;
+    private int numberOfCupholders;
+    private int topSpeed;
+    private double zeroToSixtyMPH;
+    private String color;
+    
+
+    /**
+     * @return the vinNumber
+     */
+    public String getVinNumber() {
+        return vinNumber;
+    }
+
+    /**
+     * @param vinNumber the vinNumber to set
+     */
+    public void setVinNumber(String vinNumber) {
+        this.vinNumber = vinNumber;
+    }
+
+    /**
+     * @return the engineType
+     */
+    public String getEngineType() {
+        return engineType;
+    }
+
+    /**
+     * @param engineType the engineType to set
+     */
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    /**
+     * @return the numberOfWheels
+     */
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    /**
+     * @param numberOfWheels the numberOfWheels to set
+     */
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    /**
+     * @return the numberOfSeats
+     */
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    /**
+     * @param numberOfSeats the numberOfSeats to set
+     */
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    /**
+     * @return the cargoCapacity
+     */
+    public double getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    /**
+     * @param cargoCapacity the cargoCapacity to set
+     */
+    public void setCargoCapacity(double cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    /**
+     * @return the range
+     */
+    public int getRange() {
+        return range;
+    }
+
+    /**
+     * @param range the range to set
+     */
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    /**
+     * @return the transmissionType
+     */
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    /**
+     * @param transmissionType the transmissionType to set
+     */
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    /**
+     * @return the driveTrain
+     */
+    public String getDriveTrain() {
+        return driveTrain;
+    }
+
+    /**
+     * @param driveTrain the driveTrain to set
+     */
+    public void setDriveTrain(String driveTrain) {
+        this.driveTrain = driveTrain;
+    }
+
+    /**
+     * @return the numberOfGears
+     */
+    public int getNumberOfGears() {
+        return numberOfGears;
+    }
+
+    /**
+     * @param numberOfGears the numberOfGears to set
+     */
+    public void setNumberOfGears(int numberOfGears) {
+        this.numberOfGears = numberOfGears;
+    }
+
+    /**
+     * @return the numberOfDoors
+     */
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    /**
+     * @param numberOfDoors the numberOfDoors to set
+     */
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    /**
+     * @return the numberOfCupholders
+     */
+    public int getNumberOfCupholders() {
+        return numberOfCupholders;
+    }
+
+    /**
+     * @param numberOfCupholders the numberOfCupholders to set
+     */
+    public void setNumberOfCupholders(int numberOfCupholders) {
+        this.numberOfCupholders = numberOfCupholders;
+    }
+
+    /**
+     * @return the topSpeed
+     */
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    /**
+     * @param topSpeed the topSpeed to set
+     */
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    /**
+     * @return the zeroToSixtyMPH
+     */
+    public double getZeroToSixtyMPH() {
+        return zeroToSixtyMPH;
+    }
+
+    /**
+     * @param zeroToSixtyMPH the zeroToSixtyMPH to set
+     */
+    public void setZeroToSixtyMPH(double zeroToSixtyMPH) {
+        this.zeroToSixtyMPH = zeroToSixtyMPH;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+   
+}
